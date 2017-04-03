8591b32abc5c63a3bcda1da845be551e97a19531
notorious01 <tuborg1604@gmail.com>
2017-04-04 01:54:52 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,15 +1,151 @@
 #include "matrix.hpp"
 
-matrix_t::matrix_t() noexcept : rows_{0}, columns_{0}, elements_{nullptr}
+int Matrix::stolb_() const
 {
+	return stolb;
 }
 
-auto matrix_t::rows() -> unsigned int
+int Matrix::stroka_() const
 {
-    return rows_;
+	return stroka;
 }
 
-auto matrix_t::columns() -> unsigned int
+Matrix::Matrix(): stolb(0), stroka(0), matrix(nullptr)
+{}
+
+Matrix::Matrix(int _stolb, int _stroka)
 {
-    return columns_;
+	stolb = _stolb;
+	stroka = _stroka;
+	matrix = new int*[stroka];
+	for (int i = 0; i < stroka; ++i){
+		matrix[i] = new int[stolb];
+		for (int j = 0; j < stolb; ++j){
+			matrix[i][j] = 0;
+		}
+	}
+}
+
+Matrix::Matrix(const Matrix& result)
+{
+	stolb = result.stolb;
+	stroka = result.stroka;
+	matrix = new int*[stroka];
+	for (int i = 0; i < stroka; ++i){
+		matrix[i] = new int[stolb];
+		for (int j = 0; j < stolb; ++j){
+			matrix[i][j] = result.matrix[i][j];
+		}
+	}
+}
+
+Matrix::~Matrix()
+{
+	for (int i = 0; i < stroka; ++i){
+		delete[]matrix[i];
+	}
+	delete[]matrix;
+}
+
+istream& operator >> (istream& infile, Matrix& result)
+{
+		for (int i = 0; i < result.stroka; i++)
+			for (int j = 0; j < result.stolb; j++)
+				infile >> result.matrix[i][j];
+		return infile;
+}
+
+void Matrix::scan(string filename) const 
+{
+	ifstream infile;
+	infile.open(filename);
+	if (!infile.is_open())
+		cout << "Error! Please, try again!" << endl;
+	else
+	{
+		for (int i = 0; i < stroka; i++){
+			for (int j = 0; j < stolb; j++){
+				infile >> matrix[i][j];
+			}
+		}
+	}
+	infile.close();
+}
+
+ostream& operator << (ostream& outfile, const Matrix& result)
+{
+	for (int i = 0; i < result.stroka; i++){
+		for (int j = 0; j < result.stolb; j++){
+			outfile << result.matrix[i][j] << " ";
+		}
+	}
+		outfile << endl;
+		return outfile;
+}
+
+bool Matrix::operator == (const Matrix& m2) const
+{
+	for (int i = 0; i < stroka; i++){
+		for (int j = 0; j < stolb; j++){
+			if (matrix[i][j] == m2.matrix[i][j]){
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
+Matrix Matrix::operator + (const Matrix& m2) const
+{
+	if ((stolb != m2.stolb) || (stroka != m2.stroka)) {
+		cout << "Error!";
+	}
+	else {
+		Matrix result(stolb, stroka);
+		for (int i = 0; i < stroka; ++i){
+			for (int j = 0; j < stolb; ++j){
+				result.matrix[i][j] = matrix[i][j] + m2.matrix[i][j];
+			}
+		}
+		return result;
+	}
+}
+
+Matrix Matrix::operator * (const Matrix& m2) const
+{
+	if (m2.stroka != stolb){
+		cout << "Error!";
+	}
+	else {
+		Matrix result(stroka, m2.stolb);
+		for (int i = 0; i < stroka; i++){
+			for (int j = 0; j < m2.stolb; j++){
+				for (int k = 0; k < stolb; k++){
+					result.matrix[i][j] += matrix[i][k] * m2.matrix[k][j];
+				}
+			}
+		}
+		return result;
+	}
+}
+
+Matrix& Matrix::operator = (const Matrix& result)
+{
+	if (&result == this){
+		return *this;
+	}
+	for (int i = 0; i < stroka; i++){
+		delete[]matrix[i];
+	}
+	delete[]matrix;
+	matrix = new int*[result.stroka];
+	for (int i = 0; i < result.stroka; i++)
+	{
+		matrix[i] = new int[result.stolb];
+		for (int j = 0; j < result.stolb; j++)
+			matrix[i][j] = result.matrix[i][j];
+	}
+	stroka = result.stroka;
+	stolb = result.stolb;
+	return *this;
 }
